603f84c8dddb30c53dff0c4e501c000ab28d30ca
paul23093 <paul23093@yandex.ru>
2021-05-31 12:35:20 +0300

    custom scrollbar

--- static/js/index.js
+++ static/js/index.js
@@ -6,6 +6,21 @@ function getAge() {
 	document.getElementById('birthday').getElementsByTagName('span')[0].innerHTML += ' (' + currentAge + ' y.o.)';
 }
 
+function getSkills() {
+	const skills = fetch("static/data/skills.json").then(function(json) {
+		console.log(json);
+	});
+	// const ul = document.getElementsByClassName('skills-content')[0];
+	// console.log(skills);
+	// for(i=0; i<len(skills['skills']); i++) {
+	// 	var li = document.createElement('li');
+	// 	li.setAttribute('class', 'skills-item');
+	// 	li.innerHTML = skills['skills'][i];
+	// 	ul.appendChild(li);
+	// }
+}
+
 function getData() {
 	getAge();
+	getSkills();
 }
