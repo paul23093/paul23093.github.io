8a48812cf4ea0ceca6b33f881eac8ace6684dea4
paul23093 <paul23093@yandex.ru>
2021-05-31 15:14:14 +0300

    custom scrollbar

--- static/js/index.js
+++ static/js/index.js
@@ -7,7 +7,8 @@ function getAge() {
 }
 
 function getSkills() {
-	fetch("static/data/content.json").then((response) => {
+	// fetch("static/data/content.json").then((response) => {
+	fetch("https://raw.githubusercontent.com/paul23093/profile/master/static/data/content.json").then((response) => {
 		response.json().then((data) => {
 			const skills = data['skills'];
 			const ul = document.getElementsByClassName('skills-content')[0];
