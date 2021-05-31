249a0a4c3252108842fd4bf51e5b2d586bffb273
paul23093 <paul23093@yandex.ru>
2021-05-31 16:35:04 +0300

    custom scrollbar

--- static/js/index.js
+++ static/js/index.js
@@ -26,13 +26,62 @@ function getJobs() {
 	// fetch("static/data/content.json").then((response) => {
 	fetch("https://raw.githubusercontent.com/paul23093/profile/master/static/data/jobs.json").then((response) => {
 		response.json().then((data) => {
-			const skills = data['skills'];
-			const ul = document.getElementsByClassName('skills-content')[0];
-			for(i=0; i<skills.length; i++) {
-				var li = document.createElement('li');
-				li.setAttribute('class', 'skills-item');
-				li.innerHTML = skills[i];
-				ul.appendChild(li);
+			const jobs = data['jobs'];
+			const experience = document.getElementsByClassName('experience')[0];
+			
+			for([i, job] of jobs.entries()) {
+				var company_block = document.createElement('div');
+				company_block.setAttribute('class', 'company-block');
+				experience.appendChild(company_block);
+				
+				var company = document.createElement('div');
+				company.setAttribute('class', 'company');
+				company_block.appendChild(company);
+				
+				var company_icon_block = document.createElement('div');
+				company_icon_block.setAttribute('class', 'company-icon-block', 'onclick', 'window.open("https://exness.com")');
+				company.appendChild(company_icon_block);
+				
+				var company_description = document.createElement('div');
+				company_description.setAttribute('class', 'description');
+				company.appendChild(company_description);
+				
+				var job_title = document.createElement('div');
+				job_title.setAttribute('class', 'title');
+				job_title.innerHTML = job['job_title'];
+				company_description.appendChild(job_title);
+
+				var company_info = document.createElement('div');
+				company_info.setAttribute('class', 'company-info');
+				company_info.innerHTML = job['company_name'] + ' - ' + job['company_desc'];
+				company_description.appendChild(company_info);
+
+				var tech = document.createElement('div');
+				tech.setAttribute('class', 'category tech');
+				company_description.appendChild(tech);
+
+				var tech_content = document.createElement('div');
+				tech_content.innerHTML = job['job_skills'].join(', ');
+				company_description.appendChild(tech_content);
+
+				var tasks = document.createElement('div');
+				tasks.setAttribute('class', 'category tasks');
+				company_description.appendChild(tasks);
+
+				var achivements = document.createElement('ul');
+				achivements.setAttribute('class', 'achivements');
+				company_description.appendChild(achivements);
+
+				for(achivement of job['job_achivements']) {
+					var achive = document.createElement('li');
+					achive.setAttribute('class', 'achive-element');
+					achive.innerHTML = achivement;
+					achivements.appendChild(achive);
+				}
+				if(i != jobs.length - 1) {
+					var hr = document.createElement('hr');
+					experience.appendChild(hr);
+				}
 			}
 		});
 	});
@@ -41,4 +90,5 @@ function getJobs() {
 function getData() {
 	getAge();
 	getSkills();
+	getJobs();
 }
