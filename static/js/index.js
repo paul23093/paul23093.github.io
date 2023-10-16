cb4b4e25055b68ee6a6f4fe02e46a713eceafc74
psemenov <pavel.semenov@exness.com>
2023-10-16 10:21:04 +0300

    Added personal info

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,7 @@ function getAge() {
 	const birthDate = new Date('1993-09-23');
 	const diffTime = Math.abs(currentDate - birthDate);
 	const currentAge = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365)); 
-	document.getElementById('birthday').getElementsByTagName('span')[0].innerHTML += currentAge + ' y.o.';
+	document.getElementById('birthday').getElementsByTagName('span')[1].innerHTML += currentAge + ' y.o.';
 }
 
 function getSkills() {
