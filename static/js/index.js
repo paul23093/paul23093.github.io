42291e1fee3cac033f0c6eb203d5c55f86ea7dcc
paul23093 <paul23093@yandex.ru>
2024-10-19 17:19:08 +0300

    Αdaptability improvements

--- static/js/index.js
+++ static/js/index.js
@@ -160,17 +160,3 @@ function getData() {
 	getSkills();
 	getJobs();
 }
-
-// window.addEventListener('resize', () => {
-// 	skills = document.getElementsByClassName('skills')[0];
-// 	skillsContent = document.getElementsByClassName('skills-content')[0]
-// 	if((document.documentElement.clientWidth < 900) || (document.documentElement.clientWidth/document.documentElement.clientHeight < 1)){
-// 		skills.style.display = "table";
-// 		skills.style.textAlign = "center";
-// 		skillsContent.style.width = "70vw";
-// 	} else {
-// 		skills.style.display = "table-cell";
-// 		skills.style.textAlign = "left";
-// 		skillsContent.style.width = "30vw";
-// 	}
-// });
